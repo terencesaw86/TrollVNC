ce1571f64e39eacf811e4994d5fada70a04252b3
Lessica <82flex@gmail.com>
2025-10-29 02:37:14 +0800

    chore: copyright
    Signed-off-by: Lessica <82flex@gmail.com>

--- app/TrollVNC/TrollVNC/AppDelegate.h
+++ app/TrollVNC/TrollVNC/AppDelegate.h
@@ -1,9 +1,19 @@
-//
-//  AppDelegate.h
-//  TrollVNC
-//
-//  Created by 82Flex on 9/1/25.
-//
+/*
+ This file is part of TrollVNC
+ Copyright (c) 2025 82Flex <82flex@gmail.com> and contributors
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License version 2
+ as published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #import <UIKit/UIKit.h>
 
